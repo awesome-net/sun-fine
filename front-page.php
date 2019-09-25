11d006dccc0bd4c305d12ff2b6629011d1b91872
yk-project <idle.away.ver.2.0.1@gmail.com>
2019-09-25 18:36:55 +0900

    TOPメニュー位置・フッター下部余白（IE）・業務内容画像比率（safari）修正

--- front-page.php
+++ front-page.php
@@ -18,37 +18,39 @@ get_header(); ?>
 	<main id="main" class="site-main">
 
 		<section class="top-hero">
-			<div class="top-hero__title">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/hero/top-banner-logo.svg" alt="タイトル画像：サンファイン" class="top-hero__title-pc">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/hero/tb-top-banner-logo.svg" alt="タイトル画像：サンファイン" class="top-hero__title-tb">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/hero/sp-top-banner-logo.svg" alt="タイトル画像：サンファイン" class="top-hero__title-sp">
-			</div>
+			<div class="top-hero__cont">
+				<div class="top-hero__title">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/hero/top-banner-logo.svg" alt="タイトル画像：サンファイン" class="top-hero__title-pc">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/hero/tb-top-banner-logo.svg" alt="タイトル画像：サンファイン" class="top-hero__title-tb">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/hero/sp-top-banner-logo.svg" alt="タイトル画像：サンファイン" class="top-hero__title-sp">
+				</div>
 
-			<div class="top-hero__menu">
-				<?php get_template_part('template-parts/object/home/hero-nav','pc'); ?>
-			</div>
+				<div class="top-hero__menu">
+					<?php get_template_part('template-parts/object/home/hero-nav','pc'); ?>
+				</div>
 
-			<?php
-			$hero_tel               = get_field('hero_tel', 'options');
-			$hero_field_tel_title   = get_field_object('hero_tel','options');
-			$hero_business_hours    = get_field('hero_business-hours', 'options');
-			$hero_field_business_hours_title   = get_field_object('hero_business-hours','options');
-			?>
-			<ul class="top-hero__info">
-				<li>
-					<?php echo $hero_field_tel_title['label']; ?>･･<?php echo $hero_tel; ?>
-				</li>
-				<li>
-					<?php echo $hero_field_business_hours_title['label']; ?>･･<?php echo $hero_business_hours; ?>
-				</li>
-			</ul>
+				<?php
+				$hero_tel               = get_field('hero_tel', 'options');
+				$hero_field_tel_title   = get_field_object('hero_tel','options');
+				$hero_business_hours    = get_field('hero_business-hours', 'options');
+				$hero_field_business_hours_title   = get_field_object('hero_business-hours','options');
+				?>
+				<ul class="top-hero__info">
+					<li>
+						<?php echo $hero_field_tel_title['label']; ?>･･<?php echo $hero_tel; ?>
+					</li>
+					<li>
+						<?php echo $hero_field_business_hours_title['label']; ?>･･<?php echo $hero_business_hours; ?>
+					</li>
+				</ul>
 
-			<?php
-			$hero_copyright        = get_field('copyright', 'options');
-			?>
-			<div class="top-hero__copyright">
-				<?php echo $hero_copyright; ?>
-			</div>
+				<?php
+				$hero_copyright        = get_field('copyright', 'options');
+				?>
+				<div class="top-hero__copyright">
+					<?php echo $hero_copyright; ?>
+				</div>
+		</div>
 		</section>
 
 		<section class="top-about">
